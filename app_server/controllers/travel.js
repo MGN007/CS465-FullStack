7d3196df9040d1e063de2933467a86a92593c9a3
MGN007 <matthew.garcia0314@yahoo.com>
2026-03-21 01:52:05 -0500

    Module 3 completed

--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -1,9 +1,17 @@
-// Controller function for the travel page
-// This function renders the travel view when the route is requested
+// Import the Node.js file system module to read external files
+const fs = require('fs');
+
+// Read the trips.json file and convert it from JSON text into a JavaScript object
+// This allows us to use the trip data dynamically in our application
+const trips = JSON.parse(fs.readFileSync('./data/trips.json', 'utf8'));
 
 /* GET travel view */
+// This controller function renders the travel page and passes data to the view
 const travel = (req, res) => {
-    res.render('travel', { title: 'Travlr Getaways' });
+    res.render('travel', { 
+        title: 'Travlr Getaways', // Page title
+        trips // Pass the trips data to the Handlebars template
+    });
 };
 
 // Export the controller so it can be used in the route file
